83388b7121bb21bf9cb84fa2acfea0a70b5e585c
Lukyanchuk Ignat <115699352+PhoenixGoIt@users.noreply.github.com>
2024-09-30 15:58:54 +0300

    clear

--- lib/UserProvider.tsx
+++ lib/UserProvider.tsx
@@ -2,7 +2,6 @@
 "use client"
 import { useGetUser } from "@/lib/api/auth/auth-quary"
 import { ReactNode } from "react"
-import { useUserStore } from "./store/userStore";
 
 const UserProvider = ({ children }: { children: ReactNode }) => {
   const {  isLoading, error } = useGetUser();
--- lib/store/userStore.ts
+++ lib/store/userStore.ts
@@ -1,36 +1,3 @@
-
-// "use client";
-// import { create } from 'zustand';
-// import { UserState } from '../@type';
-
-
-// const getTokenFromLocalStorage = () => {
-//   if (typeof window !== 'undefined') {
-//     return localStorage.getItem('jwtToken');
-//   }
-//   return null;
-// };
-
-// export const useUserStore = create<UserState>((set) => ({
-//   user: null,
-//   isLogin: false,
-//   token: getTokenFromLocalStorage(),
-//   login: (user) => set({ user, isLogin: true }),
-//   logout: () => set({ user: null, isLogin: false }),
-//   setToken: (token) => {
-//     if (typeof window !== 'undefined') {
-//       localStorage.setItem('jwtToken', token);
-//     }
-//     set({ token });
-//   },
-//   removeToken: () => {
-//     if (typeof window !== 'undefined') {
-//       localStorage.removeItem('jwtToken');
-//     }
-//     set({ token: null });
-//   },
-// }));
-// stores/useUserStore.ts
 "use client";
 import { create } from 'zustand';
 import { devtools, persist } from 'zustand/middleware';
